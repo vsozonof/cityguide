02c3d3d0b7f937c39df6c58ff13ad6920e6e8b51
Victor <vr.sozonoff@gmail.com>
2024-10-23 18:29:06 +0200

    react time

---
+++ cityguide-ui/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+
+function App()
+{
+  return (
+    <div className="cityguide-modal">
+		<div className="modal-header">
+		</div>
+		<div className="modal-content">
+		</div>
+	</div>
+  );
+}
+
+export default App;
